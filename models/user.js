a0a957c7e480a8c95347368e59689c2be4acb85f
getwie <f95130687@gmail.com>
2024-03-26 21:39:38 +0800

    first commit

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../util/database');
+const Project = require('./project');
+const Threads = require('./threads');
+const Threads_Message = require('./threads_message');
+const daily_personal = require('./daily_personal');
+
+const User = sequelize.define('user', {
+    username: {
+        type: DataTypes.TEXT,
+        allowNull:false
+    },
+    account: {
+        type: DataTypes.TEXT,
+        allowNull:false
+    },
+    password: {
+        type: DataTypes.TEXT,
+        allowNull:false
+    },
+    role: {
+        type: DataTypes.TEXT,
+        allowNull:false
+    },  
+});
+
+
+User.belongsToMany(Project, {through:"User_Projects"});
+Project.belongsToMany(User, {through:"User_Projects"});
+
+User.hasMany(Threads_Message);
+User.hasMany(Threads);
+
+User.hasMany(daily_personal);
+
+module.exports = User;
