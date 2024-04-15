6af53b2743cbf5c2cc07cefc76888a1fafb5a7c4
getwie <f95130687@gmail.com>
2024-04-15 16:16:38 +0800

    chatroomHistoryOK

--- controllers/chatroom.js
+++ controllers/chatroom.js
@@ -5,17 +5,6 @@ exports.getChatroomHistory = async (req, res) => {
     const projectId = req.params.projectId;
     console.log("Chatroom_message",Chatroom_message); // 查看输出，确保它不是 undefined
 
-    // try {
-    //     const messages = await Chatroom_message.findAll({
-    //         where: { projectId: projectId },
-    //         order: [['createdAt', 'ASC']]
-    //     });
-    //     res.json(messages);
-    // } catch (error) {
-    //     console.log(error)
-    //     res.status(500).send({ message: '获取历史消息失败', error: error });
-    // }
-
     await Chatroom_message.findAll({
         where: { projectId: projectId },
         order: [['createdAt', 'ASC']]
